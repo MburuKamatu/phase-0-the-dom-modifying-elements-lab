dd85027999607b5792ff286da49fb44ce9f1246d
+ <+>
2022-08-25 23:04:53 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const ul = document.getElementsByTagName("main")[0];
+ul.remove()
+let newHeader = document.createElement("h1");
+newHeader.id="victory"
+newHeader.textContent="YOUR-NAME is the champion"
+
